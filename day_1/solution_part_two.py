98916138f0c3d67fa5c4ec3235cbfab3ce9c1af3
drwadu <dominik.rusovac@hotmail.com>
2020-12-03 10:23:25 +0100

    Add python solution to day_1.

---
+++ day_1/solution_part_two.py
@@ -0,0 +1,2 @@
+with open('input.txt', 'r') as f:
+    print(list(filter(lambda l: bool(l), (lambda es: [i*j*k if i+j+k == 2020 else 0 for i in es for j in es for k in es])(list(map(int, f.readlines())))))[0])
