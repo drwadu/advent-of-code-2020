2a9e331335812f099c8af3deeb549da441a1d4a5
drusovac <dominik.rusovac@tu-dresden.de>
2021-04-25 19:34:38 +0200

    Satisfy conventions.

--- day-1/lisp/common-lisp/solution_part_one.lisp
+++ day-1/lisp/common-lisp/solution_part_one.lisp
@@ -1,9 +1,7 @@
 (require "asdf")
 
-(defvar ints
-  (remove-duplicates
-   (mapcar 'parse-integer (uiop:read-file-lines "../../input.txt"))))
+(defvar *ints*
+   (mapcar 'parse-integer (uiop:read-file-lines "../../input.txt")))
 (print (reduce '* (remove-if-not (lambda (x)
-			    (some (lambda (y) (= 2020 (+ x y))) ints))
-			  ints)))
-
+				   (some (lambda (y) (and (= 2020 (+ x y)) (/= x y))) *ints*))
+				 *ints*)))
