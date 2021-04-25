f47b73fb7b1b92493361bca5900b80cdfd08889b
drusovac <dominik.rusovac@tu-dresden.de>
2021-04-25 21:24:20 +0200

    Add haskell solutions.

--- day-1/haskell/solution_part_two.hs
+++ day-1/haskell/solution_part_two.hs
@@ -2,4 +2,4 @@ main :: IO ()
 main = do
    input <- lines <$> readFile "../input.txt"
    let xs = map read input :: [Int]
-   print $ head [x*x' | x<-xs, x'<-xs, x+x'==2020] 
+   print $ head [x*x'*x'' | x<-xs, x'<-xs, x''<-xs, x+x'+x''==2020] 
